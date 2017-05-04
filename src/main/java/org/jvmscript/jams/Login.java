e287d53f91c3b10f5c77adfc85da8ad9830fd4b8
leon.ferguson <leon.ferguson@corclearing.com>
2017-05-04 07:36:32 -0700

    Added JamsUtility

---
+++ src/main/java/org/jvmscript/jams/Login.java
@@ -0,0 +1,9 @@
+package org.jvmscript.jams;
+
+/**
+ * Created by leon.ferguson on 5/3/2017.
+ */
+public class Login {
+    public String username;
+    public String password;
+}
---
+++ src/main/java/org/jvmscript/jams/LoginResponse.java
@@ -0,0 +1,9 @@
+package org.jvmscript.jams;
+
+/**
+ * Created by leon.ferguson on 5/3/2017.
+ */
+public class LoginResponse {
+    public String access_token;
+    public String expires_in;
+}
