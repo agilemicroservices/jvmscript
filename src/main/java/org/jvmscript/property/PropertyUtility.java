ae8f49f6d0f91b9ec18fe46ffa31b05180554911
agilemicroservice <agilemicroservices@gmail.com>
2020-11-10 07:02:06 -0500

    Added property file output to PropertyUtility

--- src/main/java/org/jvmscript/property/PropertyUtility.java
+++ src/main/java/org/jvmscript/property/PropertyUtility.java
@@ -1,5 +1,7 @@
 package org.jvmscript.property;
 
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Properties;
@@ -15,8 +17,24 @@ public class PropertyUtility {
         inputStream.close();
     }
 
+    public static void propertyReadFile(String propertyFile) throws Exception {
+        properties = new Properties();
+        var fileInputStream = new FileInputStream(propertyFile);
+        properties.load(fileInputStream);
+        fileInputStream.close();
+    }
+
+    public static void propertyWriteFile(String propertyFile, String comments) throws Exception {
+        var fileOutputStream = new FileOutputStream(propertyFile);
+        properties.store(fileOutputStream, comments);
+        fileOutputStream.close();
+    }
+
     public static String propertyGet(String propertyName) {
         return properties.getProperty(propertyName);
     }
+    public static void propertyPut(String propertyName, Object value) {properties.put(propertyName, value);}
+    public static void propertyClear() {properties.clear();;}
+    public static void propertyInitialzie() {properties = new Properties();}
 
 }
